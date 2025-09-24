317d52048eaa4992e7ad5c2b80b2d6ebe5bcbbba
tomasusan <144578297+tomasusan@users.noreply.github.com>
2025-09-24 16:45:52 +0800

    Init Update

--- main.cpp
+++ main.cpp
@@ -1,27 +1,4 @@
-#include <iostream>
-#include "EventManagement.h"
-#include "TestA.h"
-using namespace std;
-
-void TestEvent() {
-    const auto Event = EventManagement::GetInstance();
-    Event->AddEventListener("TestEvent", []() {
-       cout<<"TestEvent Triggered\n";
-    });
-}
-
-void TestEventParameter() {
-    const auto Event = EventManagement::GetInstance();
-    Event->AddEventListener("Parameter", function([](int x, int y) {
-        cout << x << " " << y << "\n";
-    }));
-}
-
 int main() {
 
-    TestEventParameter();
-
-    const auto Event = EventManagement::GetInstance();
-    Event->DispatchEvent("Parameter", 1, 2);
     return 0;
 }
