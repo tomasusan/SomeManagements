80fe4d1e6ebea55e918d88d8fa17afd1a9c11ee5
taoyu.zhuang <taoyu.zhuang@appshahe.com>
2025-09-29 19:10:48 +0800

    findway

--- AStar/AStarMatrix.h
+++ AStar/AStarMatrix.h
@@ -1,17 +1,47 @@
 #pragma once
 #include "Matrix.h"
 #include "LogManagement.h"
+#include <queue>
+using namespace std;
+
+struct State {
+    pair<int, int> CurPosition;
+    pair<int, int> EndPosition;
+
+    int ToDistance() const {
+        return abs(CurPosition.first - EndPosition.first) + abs(CurPosition.second - EndPosition.second);
+    }
+
+    bool operator>(const State &Other) const {
+        return Other.ToDistance() < ToDistance();
+    }
+
+    bool operator<(const State &Other) const {
+        return ToDistance() < Other.ToDistance();
+    }
+};
+
+enum NextDirection {
+    Right = 1,
+    Left = -1,
+    Up = -1,
+    Down = 1
+};
 
 class AStarMatrix : public Matrix<char> {
 public:
     AStarMatrix(int InRow, int InCol, const std::pair<int, int> &InStart, const std::pair<int, int> &InEnd,
                 bool LoadDefault);
 
+    virtual void ShowMatrix() override;
+    void FindWay();
 private:
     std::pair<int, int> Start, End;
 
+    priority_queue<State> StateQueue;
     void LoadDefaultMatrix();
-    virtual void ShowMatrix() override;
+    NextDirection NextVertical = Up;
+    NextDirection NextHorizontal = Right;
 };
 
 inline AStarMatrix::AStarMatrix(int InRow, int InCol, const std::pair<int, int> &InStart,
@@ -46,8 +76,38 @@ inline void AStarMatrix::LoadDefaultMatrix() {
             if (DefaultMatrix[i][j] == 'S') Start = {i, j};
             if (DefaultMatrix[i][j] == 'E') End = {i, j};
         }
+
+    NextVertical = End.first - Start.first > 0 ? Down: Up;
+    NextHorizontal = Start.second - End.second > 0 ? Right: Left;
+
 }
 
 inline void AStarMatrix::ShowMatrix() {
     Matrix<char>::ShowMatrix();
+}
+
+inline void AStarMatrix::FindWay() {
+    State StartState{Start, End};
+    StateQueue.push(StartState);
+    while (!StateQueue.empty()) {
+        const auto CurState = StateQueue.top();
+        StateQueue.pop();
+
+        auto StateVertical = CurState;
+        StateVertical.CurPosition.first += NextVertical;
+
+        auto StateHorizontal = CurState;
+        StateHorizontal.CurPosition.second += NextHorizontal;
+
+        auto StateSlating = CurState;
+        StateSlating.CurPosition.first += NextVertical;
+        StateSlating.CurPosition.second += NextHorizontal;
+
+        if (*GetAt(StateVertical.CurPosition.first, StateVertical.CurPosition.second) != '#') StateQueue.push(StateVertical);
+        if (*GetAt(StateHorizontal.CurPosition.first, StateHorizontal.CurPosition.second) != '#') StateQueue.push(StateHorizontal);
+        if (*GetAt(StateSlating.CurPosition.first, StateSlating.CurPosition.second) != '#') StateQueue.push(StateSlating);
+
+        
+
+    }
 }
