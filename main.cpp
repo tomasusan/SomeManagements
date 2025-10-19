c3b708658e733794f61b4f7440219177400420e5
taoyu.zhuang <taoyu.zhuang@appshahe.com>
2025-10-19 13:31:53 +0800

    屏蔽根节点视图入口

--- main.cpp
+++ main.cpp
@@ -5,10 +5,5 @@ using namespace std;
 
 int main() {
     auto cell = new CellDetect(true);
-    cout << "Would you like to only see the root node? Y/N" <<endl;
-    char ye;
-    cin >>ye;
-    bool seeOnlyRoot = false;
-    seeOnlyRoot = ye == 'y' || ye == 'Y';
-    cell->DetectCells(seeOnlyRoot);
+    cell->DetectCells();
 }
